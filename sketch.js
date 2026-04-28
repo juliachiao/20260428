52a785583c07f87029b4faa446767c6d11cfc16f
juliachiao <julia.jih.0507@gmail.com>
2026-04-28 11:27:33 +0800

    線條特效

--- sketch.js
+++ sketch.js
@@ -35,6 +35,24 @@ function draw() {
   if (hands.length > 0) {
     for (let hand of hands) {
       if (hand.confidence > 0.1) {
+        // 依照要求的編號利用 line() 將關節點串接起來繪製手指骨架
+        stroke(0, 255, 0); // 設定線條為綠色
+        strokeWeight(3);   // 設定線條粗細
+        let fingers = [
+          [0, 1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 16],
+          [17, 18, 19, 20]
+        ];
+        for (let finger of fingers) {
+          for (let j = 0; j < finger.length - 1; j++) {
+            let p1 = hand.keypoints[finger[j]];
+            let p2 = hand.keypoints[finger[j + 1]];
+            line(p1.x, p1.y, p2.x, p2.y);
+          }
+        }
+
         // Loop through keypoints and draw circles
         for (let i = 0; i < hand.keypoints.length; i++) {
           let keypoint = hand.keypoints[i];
